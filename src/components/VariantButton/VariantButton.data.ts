4ef0884cc1353efb22d33ed761ec3d203820b3fe
atanasster <atanasster@gmail.com>
2021-05-09 07:10:05 +0300

    update data-driven testing

---
+++ src/components/VariantButton/VariantButton.data.ts
@@ -0,0 +1,24 @@
+export default {
+  overview: {
+    "0": {
+      text: "Destiny Funk",
+      icon: "Daniella Von"
+    },
+    "1": {
+      text: "Terrence Wyman",
+      icon: "Mazie Sawayn"
+    },
+    "2": {
+      text: "Ilene Swift",
+      icon: "Alanna Koepp"
+    },
+    "3": {
+      text: "Lukas Murazik",
+      icon: "Cassie Kerluke"
+    },
+    "4": {
+      text: "Mckenzie Hauck",
+      icon: "Mr. Micah Hagenes"
+    }
+  }
+};
