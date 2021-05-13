eb6145649ccc1e215b8a8ff91f95e8c1a2a63009
atanasster <atanasster@gmail.com>
2021-05-13 12:48:12 +0300

    clean up config

--- .config/buildtime.js
+++ .config/buildtime.js
@@ -1,14 +1,4 @@
-const { defaultBuildConfig } = require("@component-controls/core");
-
 module.exports = {
   stories: ["../src/docs/*.mdx", "../src/components/**/*.docs.tsx"],
-  pages: {
-    story: {
-      tabs: {
-        ...defaultBuildConfig.pages.story.tabs,
-        test: "@component-controls/pages/TestingPage",
-      },
-    },
-  },
   siteUrl: `https://webpack-5-controls-starter.netlify.app`,
 };
